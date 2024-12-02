db39e10a54145ffb783989e02d5cc8f350eeccde
MgsParthiban <parthitk2006@gmail.com>
2024-12-02 14:39:09 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
-echo "the current folder path $(pwd)"
-ls -lrt
-read -p "Enter the docekr image name:" Image_name
-read -p "Enter the docekr image tag:" tag
 
+image_name=$1
+
+build_number=${BUILD_NUMBER:-"latest"}
+
+image_tag="$image_name:$build_number"
+
+
+# Find the Dockerfile in subdirectories
+dockerfile_path=$(find . -type f -name "Dockerfile")
+
+# Get the directory containing the Dockerfile
+dockerfile_dir=$(dirname "$dockerfile_path")
+
+# Navigate to the directory
+cd "$dockerfile_dir" || exit
+
+# Print the path of the Dockerfile
+echo "Dockerfile found in: $(pwd)"
+
+# Execute the docker build command
+docker build -t $image_tag.
+
+echo "Docker image built with tag: $image_tag"
 
