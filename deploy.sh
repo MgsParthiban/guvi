dccb62f39b76ae6910c188e94142f1f9742675a3
Parthiban S <parthiea430@gmail.com>
2024-12-02 09:35:29 +0530

    Merge unrelated histories

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,18 @@ source ./secret.txt
 # Login to Docker Hub using the credentials
 echo "$PASSWORD" | docker login -u "$USERNAME" --password-stdin
 
+#Image_name=read -p "Enter the image name:"
+# Find the Dockerfile in subdirectories
+dockerCompose_path=$(find . -type f -name "docker-compose.yml")
+
+# Get the directory containing the Dockerfile
+dockerCompose_dir=$(dirname "$dockerCompose_path")
+
+# Navigate to the directory
+cd "$dockerCompose_dir" || exit
+
+# Print the path of the Dockerfile
+echo "Dockercompose found in: $(pwd)"
+
 # Start the Docker Compose services
 docker-compose up -d
