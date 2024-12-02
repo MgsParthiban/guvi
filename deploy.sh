ce719fb3f4aa3535ec722a2710047e3368f18a29
MgsParthiban <parthitk2006@gmail.com>
2024-12-02 10:54:21 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Load secrets from the file
-source ./secret.txt
+#source ./secret.txt
 
 # Login to Docker Hub using the credentials
-echo "$PASSWORD" | docker login -u "$USERNAME" --password-stdin
+#echo "$PASSWORD" | docker login -u "$USERNAME" --password-stdin
 
 #Image_name=read -p "Enter the image name:"
 # Find the Dockerfile in subdirectories
