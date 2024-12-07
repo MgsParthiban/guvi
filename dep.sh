47713b1866c1de6850a4f938d665748173ce9e4e
MgsParthiban <parthitk2006@gmail.com>
2024-12-07 08:13:47 +0530

    Update dep.sh

--- dep.sh
+++ dep.sh
@@ -11,34 +11,4 @@ echo $Image_name
 #image=$(echo "$Image_name" | grep -oE "[^ ]+:[^ ]+" | tail -n 1 | awk '{print }')
 #echo $image
 dockerId=$(echo "$Image_name" | awk -F '/' '{print $2}')
-private_repo=$(echo "$dockerId/d2k")
-repo_name=$(echo "$Image_name" | awk -F '/' '{ print $3}' | awk -F ':' '{print $1}')
-tag=$(echo "$Image_name" | awk -F ':' '{print $2}')
-
-#echo "this si the docker id:" $image
-echo "this si the docker id:"$dockerId
-echo "this si the docker id:" $repo_name
-echo "this si the docker id:" $tag
-echo "this si the docker id:"$private_repo
-
-#echo "$PASSWORD" | docker login -u "$USERNAME" --password-stdin --debug
-
-echo "$PASSWORD" | docker login -u "$USERNAME" --password-stdin
-
-dockerCompose_path=$(find . -type f -name "docker-compose.yml")
-
-# Get the directory containing the Dockerfile
-dockerCompose_dir=$(dirname "$dockerCompose_path")
-
-# Navigate to the directory
-cd "$dockerCompose_dir" || exit
-
-# Print the path of the Dockerfile
-echo "Dockercompose found in: $(pwd)"
-
-# Start the Docker Compose services
-docker-compose --env-file .env up -d
-
-docker tag $image $private_repo:$tag
-
-docker push $private_repo:$tag
+echo "The dockerhub user Id is : $dockerId"
