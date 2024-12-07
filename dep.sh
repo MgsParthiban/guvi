0d31a5ead29a14f3bec84b715b5f304d7fb6ea73
MgsParthiban <parthitk2006@gmail.com>
2024-12-07 08:22:36 +0530

    Update dep.sh

--- dep.sh
+++ dep.sh
@@ -2,6 +2,8 @@
 
 source ./secret.txt
 
+source ./.env
+
 # Pull the Docker image and store the output
 Image_name=$dockerimage
 
@@ -10,5 +12,5 @@ echo $Image_name
 # Extract the tag from the image name line
 #image=$(echo "$Image_name" | grep -oE "[^ ]+:[^ ]+" | tail -n 1 | awk '{print }')
 #echo $image
-dockerId=$(echo "$Image_name" | awk -F '/' '{print $2}')
+dockerId=$(echo "$Image_name" | awk -F '/' '{print $1}')
 echo "The dockerhub user Id is : $dockerId"
