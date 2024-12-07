fd20d8dd41ff169bbccb05e8fe99bcceb49408a5
MgsParthiban <parthitk2006@gmail.com>
2024-12-07 07:41:45 +0530

    Update bu.sh

--- bu.sh
+++ bu.sh
@@ -13,10 +13,14 @@ build_number=${BUILD_NUMBER:-"latest"}
 
 image_tag="$image_name:$build_number"
 
+echo "$image_tag"
+
 dockerfile_path=$(find . -type f -name "Dockerfile")
 
 # Get the directory containing the Dockerfile
 dockerfile_dir=$(dirname "$dockerfile_path")
+
+echo "$dockerfile_dir"
 
 # Navigate to the directory
 cd "$dockerfile_dir" || exit
